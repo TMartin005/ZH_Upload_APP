738d846665e17fb356fcaf4fca454b2786e1d269
StroyeR1 <martin005tm@gmail.com>
2025-08-27 18:43:38 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const studentRoutes = require('./routes/student');
+const teacherRoutes = require('./routes/teacher');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js server!');
-});
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use(express.static('uploads'));
 
+// Routes
+app.use('/api/students', studentRoutes);
+app.use('/api/teachers', teacherRoutes);
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
