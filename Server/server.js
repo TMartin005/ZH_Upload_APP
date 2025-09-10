899f0b2cfc9be624460ffd5292e52c0ed0fe3be6
StroyeR1 <martin005tm@gmail.com>
2025-09-10 15:50:49 +0200

    Add authentication routes to server
    Integrated authRoutes and dotenv configuration into the Express server. The /api/auth endpoint is now available for authentication-related requests.

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+require('dotenv').config();
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const studentRoutes = require('./routes/student');
 const teacherRoutes = require('./routes/teacher');
 const zhRoutes = require('./routes/zh_types');
+const authRoutes = require('./routes/auth');
 const path = require('path');
 const app = express();
 const PORT = 3000;
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'uploads'), { extensions: ['c', 'cpp
 app.use('/api/students', studentRoutes);
 app.use('/api/teachers', teacherRoutes);
 app.use('/api/zh_types', zhRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
